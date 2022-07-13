8c659b9fdbbcaa7a99bd1064c34d9e416424b374
Akhil Goel <akhil1goel@gmail.com>
2022-07-13 19:17:26 +0530

    Databases and Migrations Understood

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,30 @@
 import imp
 from turtle import title
 from django.shortcuts import render
+from .models import Post
 
 # from django.http import HttpResponse
 
 # Create your views here.
 
-posts = [
-    {
-        "author": "Akhil Goel",
-        "title": "Blog Post 1",
-        "content": "First Post content",
-        "date_posted": "12 July 2022",
-    },
-    {
-        "author": "Aaryan Garg",
-        "title": "Blog Post 2",
-        "content": "Second Post content",
-        "date_posted": "13 July 2022",
-    },
-]
+# posts = [
+#     {
+#         "author": "Akhil Goel",
+#         "title": "Blog Post 1",
+#         "content": "First Post content",
+#         "date_posted": "12 July 2022",
+#     },
+#     {
+#         "author": "Aaryan Garg",
+#         "title": "Blog Post 2",
+#         "content": "Second Post content",
+#         "date_posted": "13 July 2022",
+#     },
+# ]
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
